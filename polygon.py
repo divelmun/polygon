d043d5c877e60c8bdb02db3ca5d48aff667ef226
divelmun <divelmun@gmail.com>
2021-12-02 01:30:42 +0300

    add time

--- polygon.py
+++ polygon.py
@@ -1,31 +1,36 @@
-import math, os
+import math, os, time
 
 def polygon():
     num_n = int(input("Введите количество сторон: "))
     num_s = float(input("Введите длину стороны: "))
+    time_begin = time.time()
     def the_bomb(n_sides, length):
         area = (n_sides * (length ** 2)) / (4 * math.tan((math.pi) / n_sides))
-        print('Результат: ',area)
+        print('Результат: ',area,'\n')
     the_bomb(num_n, num_s)
+    print ('Время выполнения функции: ', float(time.time()-time_begin))
 
 def summ():
-    N=int(input('Введите число N: '))
-    rez=int(0)
-    for number in range(N):
-        rez=rez+number+1
-    print('Сумма первых ',N,'положительных чисел = ', rez)
+    count_n = int(input('Введите число первых N положительных чисел: '))
+    time_begin = time.time()
+    result_n = int(0)
+    for number in range(count_n):
+        result_n = result_n + number + 1
+    print('Сумма первых ',count_n,'положительных чисел = ', result_n,'\n')
+    print ('Время выполнения функции: ', float(time.time()-time_begin))
 
 print(
     'Здравствуйте! Нажмите ..\n' 
     'P - для расчета многоугольника\n' 
     'S - для расчета суммы первых N положительных чисел\n'
-    'Любую другую литеру - для завершения программы\n')
+    'Любую другую литеру - для завершения программы\n'
+    )
 key = input ()
 if key == 'p': 
-    print ('Выполняется функция расчета площади правильного многоугольника\n')
+    print ('Была вызвана функция расчета площади правильного многоугольника\n')
     polygon ()
 elif key == 's': 
-    print ('Выполняется функция для расчета суммы первых N положительных чисел\n')
+    print ('Была вызвана функция расчета суммы первых N положительных чисел\n')
     summ()
 else: 
     print ('Осуществлен выход из программы')
